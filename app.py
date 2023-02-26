c069da9e08d1d422435d51e969da2a6f135695c8
Utpal Mishra <76061500+Utpal-Mishra@users.noreply.github.com>
2023-02-26 06:00:53 +0530

    Running Web Application

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
+import json
+import pickle
 import numpy as np
 import pandas as pd
-import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 
-app = Flask(__name__) # define the application
+app=Flask(__name__)
 
-Model = pickle.load(open('RegModel.pkl', 'rb')) # Load
-Scaling = pickle.load(open('Scaling.pkl', 'rb'))
+## Load the model
+Model  = pickle.load(open('RegModel.pkl','rb'))
+Scalar = pickle.load(open('Scaling.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/PredictAPI', methods = ['POST'])
-
-
+@app.route('/PredictAPI',methods=['POST'])
 def PredictAPI():
-    data = request.json['data'] # data extracted from json file will be in key, value pair
+    data = request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
 
-    print(np.array(list(data.values())).reshape(1, -1))
-    NewData = Scaling.transform(np.array(list(data.values())).reshape(1, -1))
+    Data = Scalar.transform(np.array(list(data.values())).reshape(1,-1))
 
-    Output = Model.predict(NewData)
-    
+    Output = Model.predict(Data)
+    print(Output[0])
+
     return jsonify(Output[0])
 
-if __name__ == "__main__":
-    app.run(debug =  True)
+@app.route('/Predict',methods=['POST'])
+def Predict():
+    Data=[float(x) for x in request.form.values()]
+
+    Input = Scalar.transform(np.array(Data).reshape(1,-1))
+    print(Input)
+
+    output = Model.predict(Input)[0]
+
+    return render_template("home.html", prediction_text="Predicted Price: {}".format(round(output, 3)))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
